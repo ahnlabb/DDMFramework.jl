c1fe2288c3084a91d9683b16977577864d42cc27
Documenter.jl <documenter@juliadocs.github.io>
2022-04-14 14:27:14 +0000

    build based on 6b5da75

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DDMFramework","category":"page"},{"location":"#DDMFramework","page":"Home","title":"DDMFramework","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DDMFramework.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DDMFramework]","category":"page"}]
+}
